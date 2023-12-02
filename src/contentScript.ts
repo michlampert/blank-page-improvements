6f05c0e3bd5d99510ee6fab2325d472f24fdca7e
Michal Lampert <michlampert@gmail.com>
2023-12-02 02:52:57 +0100

    Add other pages list

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,5 +1,3 @@
-'use strict';
-
 // Content script file will run in the context of web page.
 // With content script you can manipulate the web pages using
 // Document Object Model (DOM).
@@ -11,12 +9,52 @@
 // For more information on Content Scripts,
 // See https://developer.chrome.com/extensions/content_scripts
 
-// Log `title` of current active web page
-const pageTitle: string =
-  document.head.getElementsByTagName('title')[0]?.innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
+window.addEventListener("load", updateLinks)
+
+function createComponent(names: string[]): HTMLElement {
+  const container = document.createElement("div")
+  container.id = "improvements-other-pages"
+
+  const p = document.createElement("p")
+  p.innerHTML = "Other blank pages you've been working on:"
+  container.append(p)
+
+  container.append(
+    ...names.toSorted()
+      .map(name => {
+        if (name == window.location.pathname) {
+          return name
+        }
+
+        const a = document.createElement("a")
+        a.innerHTML = name
+        a.href = window.location.origin + name
+        return a
+      })
+      .flatMap(a => ["↳ ", a, document.createElement("br")])
+  )
+
+  return container
+}
+
+function getPages(): string[] {
+  const re = /write-pageContent-(.*)/
+
+  return Object.keys(window.localStorage)
+    .map(key => re.exec(key)?.[1])
+    .filter((name): name is string => !!name)
+}
+
+function updateLinks() {
+  const sidebar = document.getElementById("sidebar-body")
+
+  if (sidebar) {
+    document.getElementById("improvements-other-pages")?.remove()
+    const container = createComponent(getPages())
+    sidebar.appendChild(container)
+  }
+
+}
 
 // Communicate with background file by sending a message
 chrome.runtime.sendMessage(
