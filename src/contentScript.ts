eecd8e04b454da460ec0bda3c8a301ca88afc6b5
Michal Lampert <michlampert@gmail.com>
2023-12-03 08:18:26 +0100

    Add character counter feature

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,53 +1,5 @@
+import { updateLinks } from './otherFiles';
 import { Settings, restoreSettings } from './storage';
-
-function createComponent(names: string[]): HTMLElement {
-  const container = document.createElement('div');
-  container.id = 'improvements-other-pages';
-
-  const p = document.createElement('p');
-  p.innerHTML = "Other blank pages you've been working on:";
-  container.append(p);
-
-  container.append(
-    ...names
-      .toSorted()
-      .map((name: string): string | HTMLAnchorElement => {
-        if (name == window.location.pathname) {
-          return name;
-        }
-
-        const a = document.createElement('a');
-        a.innerHTML = name;
-        a.href = window.location.origin + name;
-        return a;
-      })
-      .flatMap((a: string | HTMLAnchorElement) => ['↳ ', a, document.createElement('br')])
-  );
-
-  return container;
-}
-
-function getPages(filtered: boolean = false): string[] {
-  const re = /write-pageId-(.*)/;
-
-  return Object.keys(window.localStorage)
-    .map((key) => re.exec(key)?.[1])
-    .filter((name): name is string => !!name)
-    .filter(
-      (name) =>
-        !filtered || name == '/' || name == window.location.pathname || window.localStorage[`write-pageContent-${name}`]
-    );
-}
-
-function updateLinks(settings: Settings) {
-  document.getElementById('improvements-other-pages')?.remove();
-  const sidebar = document.getElementById('sidebar-body');
-
-  if (sidebar && settings.showOtherPages) {
-    const container = createComponent(getPages(settings.hideEmptyPages));
-    sidebar.appendChild(container);
-  }
-}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'UPDATE_SETTINGS') {
@@ -60,3 +12,78 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 window.addEventListener('load', () => restoreSettings(updateLinks));
 document.getElementById('sheet')?.addEventListener('change', () => restoreSettings(updateLinks));
+
+function getWordLimit() {
+  return Number(window.localStorage.getItem(`write-pageWordGoal-${window.location.pathname}`) || "0")
+}
+
+function getCharactersCount() {
+  return (document.getElementById("sheet")! as HTMLTextAreaElement).value.length
+}
+
+function setupProgressBar(setting: Settings) {
+  const progressBar = document.getElementsByClassName("progress-bar wordGoal")[0] as HTMLElement
+  const charactersProgressBar = progressBar.cloneNode(true) as HTMLElement
+  charactersProgressBar.classList.replace("wordGoal", "charactersGoal")
+  charactersProgressBar.style.backgroundColor = "rgb( 20, 73, 222 )"
+  progressBar.parentElement?.append(charactersProgressBar)
+}
+
+function setupMessages(setting: Settings) {
+  const label = (document.getElementsByClassName("select-option option-pageWordGoal")![0] as HTMLElement)
+    .getElementsByClassName("select-option-label")![0] as HTMLElement
+  label.innerHTML = label.innerHTML.replace("Word", "Character")
+
+  const message = document.getElementById("flash-rules")!.firstChild as HTMLElement
+  if (message) message.innerHTML = message.innerHTML.replace("words", "characters")
+}
+
+
+// Based on obvious observation that count of letters always is greater than count of words,
+// so limits will be reached earlier
+function setupCharactersFeature(setting: Settings) {
+  setupProgressBar(setting)
+  setupMessages(setting)
+}
+
+function updateProgressBar(setting: Settings) {
+  const progressBar = document.getElementsByClassName("progress-bar charactersGoal")[0] as HTMLElement
+
+  const charactersCount = getCharactersCount()
+  const wordLimit = getWordLimit()
+
+  progressBar.style.transform = `translate(${Math.min(100.0 * charactersCount / wordLimit - 100, 0)}%, 0)`
+}
+
+function updateMessages(setting: Settings) {
+  const progressAlert = document.getElementById("flash")!
+
+  const charactersCount = getCharactersCount()
+  const wordLimit = getWordLimit()
+
+  if (charactersCount == wordLimit) {
+    const message = document.createElement("span")
+    message.innerHTML = `You've reached ${wordLimit} characters`
+
+    const childs = progressAlert.childNodes
+    childs.forEach(child => progressAlert.removeChild(child))
+
+    progressAlert.append(message)
+    progressAlert.classList.remove("hidden")
+
+    setTimeout(() => {
+      progressAlert.classList.add("hidden")
+      progressAlert.removeChild(message)
+      childs.forEach(child => progressAlert.append(child))
+    }, 3000)
+  }
+}
+
+
+function updateCharacterCountProgress(setting: Settings) {
+  updateProgressBar(setting)
+  updateMessages(setting)
+}
+
+window.addEventListener('load', () => restoreSettings(setupCharactersFeature));
+document.getElementById("sheet")!.addEventListener("input", () => restoreSettings(updateCharacterCountProgress))
